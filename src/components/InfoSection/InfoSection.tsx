98443312be915d09cf35b7b02e782da16d89ae5c
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-07-19 16:27:03 -0400

    Add div to make styling for the desktop easier

--- src/components/InfoSection/InfoSection.tsx
+++ src/components/InfoSection/InfoSection.tsx
@@ -11,12 +11,14 @@ const InfoSectionData = JSON.parse(fs.readFileSync(path.resolve(FILE_PATH), "utf
 function InfoSection() {
     return (
         <article className={styles.wrapper}>
-            <h2>Limitations of BMI</h2>
-            <p>
-                Although BMI is often a practical indicator of healthy weight, it is not suited for every person.
-                Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may
-                not be beneficial to use.
-            </p>
+            <div className={styles.info}>
+                <h2>Limitations of BMI</h2>
+                <p>
+                    Although BMI is often a practical indicator of healthy weight, it is not suited for every person.
+                    Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement
+                    may not be beneficial to use.
+                </p>
+            </div>
 
             {InfoSectionData.map(({ type, content }: { type: string; content: string }) => (
                 <InfoCard key={type} type={type}>
