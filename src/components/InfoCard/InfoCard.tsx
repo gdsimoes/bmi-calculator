95efe768c9ebe636f5b2b2ba660cd9633a0d06b5
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-07-24 11:32:02 -0400

    Add calls to JSON.stringify and JSON.parse

--- src/components/InfoCard/InfoCard.tsx
+++ src/components/InfoCard/InfoCard.tsx
@@ -14,7 +14,10 @@ async function InfoCard({ type, children }: InfoCardProps) {
     return (
         <section className={`${styles.wrapper} ${styles[type]}`}>
             <header>
-                <Image src={icon} alt="" />
+                {/* I had to do this JSON stringify and parsing to remove this warning:
+                "Only plain objects can be passed to Client Components from Server Components.
+                Module objects are not supported."" */}
+                <Image src={JSON.parse(JSON.stringify(icon))} alt="" />
                 <h3>{type}</h3>
             </header>
             <p>{children}</p>
