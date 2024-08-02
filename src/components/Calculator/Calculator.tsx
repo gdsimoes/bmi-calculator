a264b8fcdba9c7141fa8522057a2ff502125732b
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-08-02 15:17:35 -0400

    Create markup for Header component

---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,7 @@
+import styles from "./Calculator.module.css";
+
+function Calculator() {
+    return <h2>Calculator</h2>;
+}
+
+export default Calculator;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import styles from "./Header.module.css";
+
+import logo from "/public/images/logo.svg";
+
+function Header() {
+    return (
+        <header className={styles.wrapper}>
+            <Image src={logo} alt="" />
+            <h1>Body Mass Index Calculator</h1>
+            <p>
+                Better understand your weight in relation to your height using our body mass index (BM) calculator.
+                While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to
+                evaluate your overall health and well-being.
+            </p>
+        </header>
+    );
+}
+
+export default Header;
