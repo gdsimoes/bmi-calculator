36ab214082f16489171076d702aa8e186351b1cd
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-08-16 14:05:24 -0400

    Add Explanation component

---
+++ src/components/Explanation/Explanation.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import styles from "./Explanation.module.css";
+
+import manEating from "/public/images/image-man-eating.webp";
+
+function Explanation() {
+    return (
+        <section className={styles.wrapper}>
+            <Image src={manEating} alt="" width={564} height={533} />
+            <div className={styles.content}>
+                <h2>What your BMI result means</h2>
+                <p>
+                    A BMI range of 18.5 to 24.9 is considered a &apos;healthy weight.&apos; Maintaining a healthy weight
+                    may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes.
+                    Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and
+                    vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for
+                    five days a week.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default Explanation;
